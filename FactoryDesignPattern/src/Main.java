102ce43b4e01825c8fa23bd19f27ea770f6032a4
Rohan Agarwal <imrohan997@gmail.com>
2023-02-15 23:36:33 +0530

    FactoryDesignPattern

---
+++ FactoryDesignPattern/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape shape = shapeFactory.getShape("RECTANGLE");
+        shape.draw();
+    }
+}
---
+++ FactoryDesignPattern/src/ShapeFactory.java
@@ -0,0 +1,13 @@
+public class ShapeFactory {
+
+    Shape getShape(String shape) {
+        switch (shape) {
+            case "CIRCLE":
+                return new Circle();
+            case "RECTANGLE":
+                return new Rectangle();
+            default:
+                return null;
+        }
+    }
+}
